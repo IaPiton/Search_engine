b5b1131b5fe36c55b16fa9ac22af3989913de4f5
Piton <Iapiton@yandex.ru>
2024-03-31 21:16:42 +0300

    v.2

---
+++ src/main/java/searchengine/config/Site.java
@@ -0,0 +1,11 @@
+package searchengine.config;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class Site {
+    private String url;
+    private String name;
+}
